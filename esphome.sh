e7d5298dd53c948ad439dd9fe34bbb1bf4bb611f
Justin Huff <jjhuff@mspin.net>
2025-10-21 21:04:28 -0700

    Starting

---
+++ esphome.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ARGS="-p 6052:6052"
+
+if [ -c "/dev/ttyUSB0" ]; then
+  ARGS+=" --device /dev/ttyUSB0"
+fi
+
+if [ -c "/dev/ttyACM0" ]; then
+  ARGS+=" --device /dev/ttyACM0"
+fi
+
+docker run -P --rm -it -v "${PWD}":/config $ARGS esphome/esphome:2025.10.2 $@
+
